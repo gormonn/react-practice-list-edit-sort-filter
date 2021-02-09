63c9ab8add9ec0f095205a11c35bd0b83abd8871
gormonn <wfuzerbar@gmail.com>
2021-02-09 14:41:02 +0300

    initial commit

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import {
+    combineReducers,
+    createStore,
+    compose,
+    applyMiddleware
+} from "redux";
+import createSagaMiddleware from "redux-saga";
+import DevTools from "./DevTools";
+import { products, productsSagas } from "./pages/products/reducer";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const rootReducer = combineReducers({
+  products
+});
+
+const store = createStore(
+  rootReducer,
+  compose(applyMiddleware(sagaMiddleware), DevTools.instrument())
+);
+
+sagaMiddleware.run(productsSagas);
+
+export default store;
